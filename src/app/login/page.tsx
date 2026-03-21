be59b80009bd07fdb2b9f2d6949cf2d0ca36c4df
Jeremy Pradel <jeremy.pradel92@gmail.com>
2026-03-21 01:33:20 +0100

    Link n'était pas importé dans ce fichier. C'est corrigé.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect, Suspense } from 'react'
+import Link from 'next/link'
 import { signIn } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { Label } from '@/components/ui/label'
